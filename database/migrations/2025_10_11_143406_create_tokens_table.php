5e722f2c0a53b4713fca541a14f5a55343a111b2
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-11 18:06:38 +0330

    set column table Token

--- database/migrations/2025_10_11_143406_create_tokens_table.php
+++ database/migrations/2025_10_11_143406_create_tokens_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
+            $table->string('token');
+            $table->timestamps('expire_time');
             $table->timestamps();
         });
     }
