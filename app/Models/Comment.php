9a3ef5466ee737da6ade3b4b972573fb02c94b16
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-11 17:59:16 +0330

    relation-ships comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -11,4 +12,12 @@ class Comment extends Model
         'user_id',
         'comment',
     ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function blog(): BelongsTo{
+        return $this->belongsTo(Blog::class, 'blog_id');
+    }
 }
