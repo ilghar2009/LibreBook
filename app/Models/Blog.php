80d4e161cf32253524ec9b52be573261542fd06c
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-11 17:53:29 +0330

    relation ships blog to them

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Psy\Util\Str;
 
 class Blog extends Model
@@ -27,5 +29,25 @@ class Blog extends Model
         static::creating(function($blog){
             $blog->blog_id = (string) \Illuminate\Support\Str::uuid();
         });
+    }
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function comments():HasMany
+    {
+        return $this->hasMany(Comment::class, 'blog_id');
+    }
+
+    public function reports():HasMany
+    {
+        return $this->hasMany(Report::class, 'blog_id');
     }
 }
