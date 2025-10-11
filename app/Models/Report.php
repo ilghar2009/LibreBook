b94163682d2ed370ae62562ec8fe5851b6762a7c
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-11 17:42:26 +0330

    change base table reports (model and migration)

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'blog_id',
+        'explanation',
+    ];
+
 }
--- database/migrations/2025_10_07_211908_create_reports_table.php
+++ database/migrations/2025_10_07_211908_create_reports_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
+            $table->string('blog_id');
+            $table->string('explanation');
             $table->timestamps();
         });
     }
