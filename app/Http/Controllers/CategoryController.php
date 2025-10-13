a60b2594035dd78ff9a8373de38d563f64207bf3
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-13 22:29:09 +0330

    provid for connection Category many to many Blog's

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,9 +15,6 @@ class CategoryController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
