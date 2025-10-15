1963158cb89bae4b62264d545c56fed65f159e98
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-15 23:22:21 +0330

    some create blogs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -27,7 +27,22 @@ class BlogController extends Controller
 
             $user = $token->user;
 
+        //check user available
+            if(!$token->is_activity)
+                return response()->json([
+                   'error' => 'Forbidden',
+                ], 403);
 
+//        $blog = Blog::creating([
+//            'category_id' => $request->category_id,
+//            'meta_title' => $request->title,
+//            'meta_description' => $request->description,
+//            'user_id' => $user->user_id,
+//            'age' => $request->age,
+//            'pdf_file' => ,
+//            'contents',
+//            'role' =>,
+//        ]);
     }
 
     /**
