7e24f02204b65bd327c20fb5ff6b37c113847649
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-21 21:22:00 +0330

    comment store function finished

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Token;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -16,19 +17,34 @@ class CommentController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        //check token
+            $token_code = $request->bearerToken();
+            $token = Token::where('token', $token_code)->first();
+
+            $user = $token->user;
+
+        //check comment
+            $request->validate([
+                'comment' => ['required', 'min:3', 'max:100'],
+                'blog_id' => ['required', 'exists:blogs,blog_id'],
+            ]);
+
+        //create new recorde
+
+            $comment = new Comment();
+            $comment->blog_id = $request->blog_id;
+            $comment->user_id = $user->user_id;
+            $comment->comment = $request->comment;
+
+            $comment->save();
+
+        return response()->json([
+            'message' => 'Comment added successfully',
+        ], 201);
     }
 
     /**
