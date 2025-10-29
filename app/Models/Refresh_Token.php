f13fdfb630580b29cda175226731c37aa40ee603
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-30 01:47:13 +0330

    change name refresh_token controller

--- app/Models/Refresh_Token.php
+++ app/Models/Refresh_Token.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class RefreshToken extends Model
+class Refresh_Token extends Model
 {
     protected $fillable = [
         'user_id',
