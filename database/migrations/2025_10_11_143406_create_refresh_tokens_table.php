150124653c4ac6fea4002cabbc5f8a85355108e8
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2025-10-29 22:31:39 +0330

    change token name

--- database/migrations/2025_10_11_143406_create_refresh_tokens_table.php
+++ database/migrations/2025_10_11_143406_create_refresh_tokens_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tokens', function (Blueprint $table) {
+        Schema::create('refresh_tokens', function (Blueprint $table) {
             $table->id();
             $table->string('user_id')->unique();
             $table->string('token')->unique();
